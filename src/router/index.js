cc36fcef90c57844997f62bb51a684b16f558639
ttHien67 <tthanhhien2k1@gmail.com>
2022-10-23 22:43:09 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import { createWebHashHistory, createRouter } from "vue-router";
+import ContactBook from '@/components/ContactBook.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'contactbook',
+        component: ContactBook,
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;
